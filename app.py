e6a1a3bdf7a9b473f1fa6fc3cb41fc948807310b
martinc <martincontrerasur@gmail.com>
2020-05-05 16:42:55 -0400

    in process

--- app.py
+++ app.py
@@ -31,6 +31,12 @@ def centroid(geometry):
         return cen_x, cen_y
     else:
         return cen_x
+#loading comunas
+with open('geojson/comunas.geojson') as json_file:
+    geojson_comunas = json.load(json_file)
+#loading comunas
+with open('geojson/regiones.geojson') as json_file:
+    geojson_regiones = json.load(json_file)
 
 
 #APP
@@ -55,8 +61,10 @@ app.layout = html.Div(children=[
                 dcc.Dropdown(id='dataset_dropdown',
                             options = [{'label':'Casos Confirmados Acumulados', 'value':'CC'},
                                         {'label':'Casos Nuevos', 'value':'CN'},
+                                        {'label':'Casos Activos', 'value':'CA'},
                                         {'label':'Zonas en Cuarentena', 'value':'ZC'},
-                                        {'label':'Casos Activos', 'value':'CA'}],
+                                        {'label':'Estadística', 'value':'EST'},
+                                        ],
                             value ='CC',
                             style={'margin-bottom':10},
                             className = 'col s12 m8 l6'),
@@ -64,7 +72,7 @@ app.layout = html.Div(children=[
                             id='comuna_dropdown',
                             options = [
                                 {'label':label, 'value':value} for label,value in zip (['Maule', 'BioBio', 'Los Lagos', 'Los Ríos'],[7,16,10,14])],
-                            value = 7,
+                            value = "",
                             className = 'col s12 m8 l6',
                             style={'margin-bottom':10, 'display':'none'})], className ='row'),
             html.Div(children= html.Div(id ='graphs'),className='row')
@@ -92,20 +100,50 @@ def cuarentena_callback(value):
 def graph_updater(dataset_value, comuna_value):
     graphs = []
 
+    if dataset_value == 'CC':
+        df = pd.read_csv("https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/input/InformeEpidemiologico/CasosAcumuladosPorComuna.csv")
+
+        fig = go.Figure(go.Choroplethmapbox(geojson=geojson_comunas, locations=df.Comuna, z=df[df.columns[-2]],
+                                    colorscale="Reds", zmin=0, zmax=100, showscale = False,
+                                    marker_opacity=0.9, marker_line_width=0.01, featureidkey='properties.comuna'))
+        fig.update_layout(mapbox_style="light", mapbox_accesstoken=token,
+                          mapbox_zoom=3, mapbox_center = {"lat": -37.0902, "lon": -72.7129}, height = 700)
+        fig.update_layout(margin={"r":0,"t":30,"l":0,"b":0}, title_text = f"Casos confirmados acumulados al {df.columns[-2]}")
+
+        #bar graph
+        df = df.dropna()
+        df = df.sort_values(by=df.columns[-2], ascending = False)[:15]
+        bar_fig = go.Figure(data=(go.Bar(y = df['Comuna'], x=df[df.columns[-2]],
+                                text = df[df.columns[-2]], orientation = 'h', textposition = 'outside')),
+                       layout = (go.Layout(xaxis={'showgrid':False, 'ticks':'', 'showticklabels':False}, yaxis={'showgrid':False})))
+
+        bar_fig.update_layout(margin={"r":0,"t":30,"l":0,"b":0}, height=700)
+        bar_fig.update_layout(title_text = f'Casos confirmados acumulados al {df.columns[-2]}| Top Comunas')
+
+        graphs.append(html.Div(dcc.Graph(
+                                        id = 'mapa',
+                                        figure = fig
+                                        ), className = 'col s12 m6 l6'))
+        graphs.append(html.Div(dcc.Graph(
+                                        id = 'scatter',
+                                        figure = bar_fig,
+                                        ), className='col s12 m6 l6'))
+
     if dataset_value== 'ZC':
         import requests
         cuarentena = 'https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/input/Cuarentenas/Cuarentenas-Geo.geojson'
         geo_cuarentena = requests.get(cuarentena).json()
         info_cuarentena = []
         for feature in geo_cuarentena['features']:
-            info_cuarentena.append({'Nombre':feature['properties']['Nombre'],
-                              'Estado':feature['properties']['Estado'],
-                              'Fecha_Inicio':dt.datetime.utcfromtimestamp(feature['properties']['FInicio']//1000).strftime("%d/%m/%Y"),
-                              'Fecha_Termino':dt.datetime.utcfromtimestamp(feature['properties']['FTermino']//1000).strftime("%d/%m/%Y"),
-                              'Detalle':feature['properties']['Detalle'],
-                              'latitude':centroid(feature['geometry']['coordinates'])[-1],
-                              'longitude':centroid(feature['geometry']['coordinates'])[0]
-                             })
+            if feature['properties']['Estado'] ==1:
+                info_cuarentena.append({'Nombre':feature['properties']['Nombre'],
+                                  'Estado':feature['properties']['Estado'],
+                                  'Fecha_Inicio':dt.datetime.utcfromtimestamp(feature['properties']['FInicio']//1000).strftime("%d/%m/%Y"),
+                                  'Fecha_Termino':dt.datetime.utcfromtimestamp(feature['properties']['FTermino']//1000).strftime("%d/%m/%Y"),
+                                  'Detalle':feature['properties']['Detalle'],
+                                  'latitude':centroid(feature['geometry']['coordinates'])[-1],
+                                  'longitude':centroid(feature['geometry']['coordinates'])[0]
+                                 })
 
         cuarentena_df = pd.DataFrame(info_cuarentena)
         #just the 1 state (active quarentine)
@@ -121,7 +159,7 @@ def graph_updater(dataset_value, comuna_value):
                                 cuarentena_df['Fecha_Termino'], cuarentena_df['Detalle']],
                       align = 'left'))
         ])
-        table.update_layout(margin={"r":10,"t":25,"l":0,"b":20}, height = 700, title = 'Zonas en Cuarentena')
+        table.update_layout(margin={"r":10,"t":30,"l":0,"b":20}, height = 700, title = 'Zonas en Cuarentena')
         #table.update_layout(plot_bgcolor =plt_background, paper_bgcolor=plt_background)
 
         #quarentine map
@@ -130,7 +168,7 @@ def graph_updater(dataset_value, comuna_value):
         map = px.scatter_mapbox(cuarentena_df, lat="latitude", lon="longitude",
                                 size= 'Estado',size_max=40, zoom=2, text = 'Nombre')
         map.update_layout(mapbox_style = 'light',mapbox_zoom=3.1, mapbox_center = {"lat": -36.8, "lon": -73.5}, title_text='Mapa Cuarentena')
-        map.update_layout(margin={"r":10,"t":25,"l":0,"b":0}, height = 700)
+        map.update_layout(margin={"r":10,"t":30,"l":0,"b":0}, height = 700)
         #map.update_layout(plot_bgcolor =plt_background, paper_bgcolor=plt_background)
 
         graphs.append(html.Div(dcc.Graph(
@@ -144,21 +182,36 @@ def graph_updater(dataset_value, comuna_value):
 
     if dataset_value == 'CA':
         df = pd.read_csv("https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/input/InformeEpidemiologico/CasosActualesPorComuna.csv")
-        #loading comunas
-        with open('geojson/comunas.geojson') as json_file:
-            geojson_comunas = json.load(json_file)
 
         fig = go.Figure(go.Choroplethmapbox(geojson=geojson_comunas, locations=df.Comuna, z=df[df.columns[-1]],
                                     colorscale="Reds", zmin=0, zmax=100, showscale = False,
                                     marker_opacity=0.9, marker_line_width=0.01, featureidkey='properties.comuna'))
-        fig.update_layout(mapbox_style="carto-positron",
+        fig.update_layout(mapbox_style="light", mapbox_accesstoken=token,
                           mapbox_zoom=3, mapbox_center = {"lat": -37.0902, "lon": -72.7129})
-        fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+        fig.update_layout(margin={"r":0,"t":30,"l":0,"b":0}, height = 700, title_text= f'Mapa Casos activos al {df.columns[-1]}')
 
+        #bar graph
+        df = df.dropna()
+        df.sort_values(by=df.columns[-1], ascending = False)
+        df = df.sort_values(by=df.columns[-1], ascending = False)[:15]
+        bar_fig = go.Figure(data=(go.Bar(y = df['Comuna'], x=df[df.columns[-1]],
+                            text = df[df.columns[-1]], orientation = 'h', textposition = 'outside')),
+                            layout = (go.Layout(xaxis={'showgrid':False, 'ticks':'', 'showticklabels':False}, yaxis={'showgrid':False})))
+
+        bar_fig.update_layout(margin={"r":0,"t":30,"l":0,"b":0}, height=700)
+        bar_fig.update_layout(title_text =f'Casos activos al {df.columns[-1]}| Top Comunas')
+        #appending
         graphs.append(html.Div(dcc.Graph(
                                         id = 'mapa',
                                         figure = fig
-                                        ), className = 'col s12 m4 l5'))
+                                        ), className = 'col s12 m6 l6'))
+        graphs.append(html.Div(dcc.Graph(
+                                        id = 'scatter',
+                                        figure = bar_fig,
+                                        ), className='col s12 m6 l6'))
+
+
+
     return graphs
 
 app.run_server(debug=True, dev_tools_hot_reload=True)  # Turn off reloader if inside Jupyter
